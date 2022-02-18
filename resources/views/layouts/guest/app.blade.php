4f37db868a1aa382918bd18fe176db4214588e0d
Juan Carlos Marchant <juan.marchant@gmail.com>
2022-02-17 21:41:11 -0300

    Add change password page

--- resources/views/layouts/guest/app.blade.php
+++ resources/views/layouts/guest/app.blade.php
@@ -4,15 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="shortcut icon" href="{{ global_asset('img/favicon.ico') }}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
 
-        <title>{{ config('app.name', 'Laravel') }} | {{ tenant('id') ?? 'Central' }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ global_asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <style>
             [x-cloak] { 
                 display: none !important;
@@ -23,7 +23,7 @@
         @stack('styles')
 
         <!-- Scripts -->
-        <script src="{{ global_asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased text-dark bg-dark" x-data="{ mode: localStorage.theme == 'dark' }">
         <div class="min-h-screen">
